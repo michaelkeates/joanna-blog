105d59b3429da82560cb1a72011e4571312cb175
michaelkeates <mail@michaelkeates.co.uk>
2023-08-09 22:05:03 +0100

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -9,7 +9,7 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Joanna's Blog2" />
+        <meta name="description" content="Joanna's Blog" />
         <meta name="author" content="Joanna" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />

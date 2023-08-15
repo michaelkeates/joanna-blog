8fb8ba1e4ac2ad26ebe36789924818c82b969930
michaelkeates <mail@michaelkeates.co.uk>
2023-08-15 18:29:44 +0100

    padding fix

--- components/message.js
+++ components/message.js
@@ -3,16 +3,6 @@ import styled from '@emotion/styled'
 const Message = styled.p`
   font-size: 18px;
   margin-top: 13px;
-
-  @media (max-width: 768px) {
-    font-size: 14px;
-    margin-top: 10px;
-  }
-
-  @media (max-width: 480px) {
-    font-size: 12px;
-    margin-top: 8px;
-  }
 `;
 
 export default Message
